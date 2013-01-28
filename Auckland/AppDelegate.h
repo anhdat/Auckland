231f1e9a0634ba7e6562fd282312d115fc411444
Dat Anh Truong <anhdat@anh-dats-pc.(none)>
2013-01-28 20:28:39 +0700

    Initial Commit

---
+++ Auckland/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Auckland
+//
+//  Created by Dat Anh Truong on 1/28/13.
+//  Copyright (c) 2013 Dat Anh Truong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Auckland/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Auckland
+//
+//  Created by Dat Anh Truong on 1/28/13.
+//  Copyright (c) 2013 Dat Anh Truong. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
