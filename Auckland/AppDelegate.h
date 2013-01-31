bf06882b28bcd803ce6e5761a34e861af1b26a2e
anhdat <mr.anhdat@gmail.com>
2013-02-01 00:04:36 +0700

    Revert "First Commit"
    This reverts commit 707198f4e3fa6ce03f40bcf9c625b0c1dda72651.

--- Auckland/AppDelegate.h
+++ Auckland/AppDelegate.h
@@ -7,54 +7,9 @@
 //
 
 #import <Cocoa/Cocoa.h>
-#import "iTunes.h"
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>{
-    NSTrackingAreaOptions trackingOptions;
-}
+@interface AppDelegate : NSObject <NSApplicationDelegate>
 
-//--------NSView--------------
 @property (assign) IBOutlet NSWindow *window;
-@property (weak) IBOutlet NSView *mainView;
-@property (weak) IBOutlet NSView *barView;
-@property (weak) IBOutlet NSView *inView;
-//-----------------------------
-
-//--------Buttons--------------
-@property (weak) IBOutlet NSButton *btnLogo;
-@property (weak) IBOutlet NSButton *btnPrevious;
-@property (weak) IBOutlet NSButton *btnPlay;
-@property (weak) IBOutlet NSButton *btnNext;
-@property (weak) IBOutlet NSButton *btnLove;
-@property (weak) IBOutlet NSButton *btnChange;
-
-- (IBAction)atnLogo:(id)sender;
-- (IBAction)atnLove:(id)sender;
-- (IBAction)atnChange:(id)sender;
-
-//-----------------------------
-
-@property (weak) IBOutlet NSTextField *textOnBar;
-
-
-//--------Images--------------
-@property (strong, nonatomic) NSImage *imgLogo;
-@property (strong, nonatomic) NSImage *imgPrevious;
-@property (strong, nonatomic) NSImage *imgPlay;
-@property (strong, nonatomic) NSImage *imgNext;
-//----------------------------
-
-//--------NSStatusBar--------------
-@property (strong, nonatomic) NSStatusBar *sysBar;
-@property (strong, nonatomic) NSStatusItem *ourBar;
-//-----------------------------
-
-//--------NSTrackingArea--------------
-@property (strong, nonatomic) NSTrackingArea *trackIt;
-
-//------------------------------------
-
-
-@property (nonatomic, strong) iTunesTrack *track;
 
 @end
