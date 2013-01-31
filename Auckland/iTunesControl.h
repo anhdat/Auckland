707198f4e3fa6ce03f40bcf9c625b0c1dda72651
anhdat <mr.anhdat@gmail.com>
2013-01-31 23:55:01 +0700

    First Commit
    Currently I built UI and iTunes Control. Lots of thing to do.

---
+++ Auckland/iTunesControl.h
@@ -0,0 +1,13 @@
+//
+//  iTunesControl.h
+//  Auckland
+//
+//  Created by Dat Anh Truong on 1/31/13.
+//  Copyright (c) 2013 Dat Anh Truong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iTunesControl : NSObject
+
+@end
