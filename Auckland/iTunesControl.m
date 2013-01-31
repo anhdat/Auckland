e6a29dd76f4eed346b395d296879e8d2a17ac6ee
anhdat <mr.anhdat@gmail.com>
2013-02-01 00:04:36 +0700

    Revert "Revert "First Commit""
    This reverts commit bd2c2491c16a78fc1daacd58ddf48382dab5696f.

---
+++ Auckland/iTunesControl.m
@@ -0,0 +1,13 @@
+//
+//  iTunesControl.m
+//  Auckland
+//
+//  Created by Dat Anh Truong on 1/31/13.
+//  Copyright (c) 2013 Dat Anh Truong. All rights reserved.
+//
+
+#import "iTunesControl.h"
+
+@implementation iTunesControl
+
+@end
